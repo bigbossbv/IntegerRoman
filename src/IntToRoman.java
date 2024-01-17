f4a0b2a82474a8435d2856448df7377d0a2c4ab3
bigbossbv <146413816+bigbossbv@users.noreply.github.com>
2024-01-16 22:28:40 -0600

    Int to Rom method added

--- src/IntToRoman.java
+++ src/IntToRoman.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class IntToRoman {
+    private final static Logger logger=Logger.getLogger(IntToRoman.class.getName());
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        logger.info("Enter the value to convert:");
+        String input=scan.nextLine();
+        if(input.matches("\\d+")){
+            int num=Integer.parseInt(input);
+            String roman=integerToRomanNumber(num);
+            logger.info(num + " in Roman numerals: " + roman);
+        }else{
+            
+        }
+
+    }
+
+    private static String integerToRomanNumber(int num) {
+        return null;
+    }
 }
