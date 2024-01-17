49926f54287fa5180a8033e2837d1b93198836b6
bigbossbv <146413816+bigbossbv@users.noreply.github.com>
2024-01-16 22:44:57 -0600

    added key values

--- src/IntToRoman.java
+++ src/IntToRoman.java
@@ -1,8 +1,26 @@
 import java.util.Scanner;
+import java.util.TreeMap;
 import java.util.logging.Logger;
 
 public class IntToRoman {
     private final static Logger logger=Logger.getLogger(IntToRoman.class.getName());
+    private final static TreeMap<Integer, String> romanNumbers=new TreeMap<>();
+
+    static{
+    romanNumbers.put(1,"I");
+    romanNumbers.put(4,"IV");
+    romanNumbers.put(5,"V");
+    romanNumbers.put(9,"IX");
+    romanNumbers.put(10, "X");
+    romanNumbers.put(40, "XL");
+    romanNumbers.put(50, "L");
+    romanNumbers.put(90, "XC");
+    romanNumbers.put(100, "C");
+    romanNumbers.put(400, "CD");
+    romanNumbers.put(500, "D");
+    romanNumbers.put(900, "CM");
+    romanNumbers.put(1000, "M");
+    }
     public static void main(String[] args){
         Scanner scan=new Scanner(System.in);
         logger.info("Enter the value to convert:");
@@ -12,12 +30,13 @@ public class IntToRoman {
             String roman=integerToRomanNumber(num);
             logger.info(num + " in Roman numerals: " + roman);
         }else{
-            
+
         }
 
     }
 
     private static String integerToRomanNumber(int num) {
+
         return null;
     }
 }
